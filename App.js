a203de46b46fffda3312b988e1dddb77e5b33e9e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-28 19:26:10 +0430

    text

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text numberOfLines={1} onPress={() => console.log("pressed")}>
+        Hello world Hello worldHello worldHello worldHello worldHello worldHello
+        worldHello worldHello world!
+      </Text>
     </SafeAreaView>
   );
 }
@@ -13,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "dodgerblue",
-    // alignItems: "center",
-    // justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
