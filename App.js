03c269e88c2fbb9d7b04dfdc06674bfd6c528c06
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-29 14:33:42 +0430

    excercise

--- App.js
+++ App.js
@@ -1,39 +1,9 @@
-import {
-  StatusBar,
-  StyleSheet,
-  SafeAreaView,
-  Platform,
-  View,
-} from "react-native";
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  // const orientation = useDeviceOrientation();
-  // const dimension = useDimension();
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          flex: 2,
-          backgroundColor: "lightblue",
-
-          // width: "100%",
-          // height: "30%",
-          // height: orientation.landscape ? "%100" : "30%"
-        }}
-      />
-      <View style={{ backgroundColor: "lightblue", flex: 1 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
+  // return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    // justifyContent: "center",
-    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
