db3c7416a1e82d72eb59edfce8abc19898842727
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-17 21:15:50 +0430

    store the logged in user

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 // eslint-disable-next-line import/namespace
 import { StatusBar } from "expo-status-bar";
@@ -8,9 +8,22 @@ import navigationTheme from "./app/navigation/navigationTheme";
 // import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
+import { jwtDecode } from "jwt-decode";
 
 export default function App() {
   const [user, setUser] = useState();
+
+  const restoreToken = async () => {
+    const token = await authStorage.getToken();
+    if (!token) return;
+
+    setUser(jwtDecode(token));
+  };
+
+  useEffect(() => {
+    restoreToken();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
