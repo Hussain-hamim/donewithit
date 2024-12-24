d4ae488915203695c657eefb249bce1179be409f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-24 21:25:41 +0430

    button

--- App.js
+++ App.js
@@ -11,7 +11,15 @@ import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SplashScreen from "expo-splash-screen";
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  Linking,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { FontAwesome5, FontAwesome6 } from "@expo/vector-icons";
 
 SplashScreen.preventAutoHideAsync();
@@ -77,35 +85,79 @@ export default function App() {
 }
 
 const App2 = () => {
+  const onContactMe = () => {
+    Linking.openURL("mailto:mohammadhussainafghan83@gmail.com");
+  };
+
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("./app/assets/camera1_full.jpg")}
-        style={{ width: "100%", height: 200 }}
-        resizeMode="stretch"
-      />
+    <ScrollView>
+      <View style={styles.container}>
+        <Image
+          source={require("./app/assets/camera1_full.jpg")}
+          style={{ width: "100%", height: 200 }}
+          resizeMode="stretch"
+        />
 
-      <Image
-        style={{
-          width: 150,
-          height: 150,
-          borderRadius: 150,
-          borderWidth: 5,
-          borderColor: "white",
-          marginTop: -75,
-        }}
-        source={require("./app/assets/hamim.jpg")}
-      />
+        <Image
+          style={{
+            width: 150,
+            height: 150,
+            borderRadius: 150,
+            borderWidth: 5,
+            borderColor: "white",
+            marginTop: -75,
+          }}
+          source={require("./app/assets/hamim.jpg")}
+        />
 
-      <Text style={{ fontSize: 30, fontWeight: "bold" }}>Hussain Hamim</Text>
-      <Text>Founder of this application</Text>
+        <Text style={{ fontSize: 30, fontWeight: "bold" }}>Hussain Hamim</Text>
+        <Text>Founder of this application</Text>
 
-      <View style={{ flexDirection: "row", marginVertical: 10, gap: 10 }}>
-        <FontAwesome6 name="github" size={24} color="black" />
-        <FontAwesome6 name="x-twitter" size={24} color="black" />
-        <FontAwesome6 name="at" size={24} color="black" />
+        <View style={{ flexDirection: "row", marginVertical: 10, gap: 10 }}>
+          <FontAwesome6 name="github" size={24} color="black" />
+          <FontAwesome6 name="x-twitter" size={24} color="black" />
+          <FontAwesome6 name="at" size={24} color="black" />
+        </View>
+
+        <Button title="Contact Me" onPress={onContactMe} />
+
+        <Text>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+          nesciunt magnam aliquam atque quo velit veniam, corrupti sunt facilis
+          perferendis dolorum nulla quasi, quas possimus voluptatem fugit Ipsam
+          optio error est rem aperiam, excepturi explicabo deleniti! Ipsam optio
+          error est rem aperiam, excepturi explicabo deleniti! Ipsam optio error
+          est rem aperiam, placeat natus quibusdam officia vel aliquid nesciunt
+          aut nemo delectus sed sit architecto assumenda provident dolorum.
+          Alias sit eaque ad excepturi explicabo deleniti! Ipsam optio error est
+          rem aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, placeat natus quibusdam officia vel aliquid nesciunt aut nemo
+          delectus sed sit architecto assumenda provident dolorum. Alias sit
+          eaque ad excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabo deleniti! Ipsam optio error est rem
+          aperiam, excepturi explicabel aliquid nesciunt aut nemo delectus sed
+          sit architecto assumenda provident dolorum. Alias sit eaque ad officia
+          magni recusandae dignissimos tenetur explicabo, est modi odio,
+          doloremque vitae sequi eveniet molestiae? Magnam nam sequi laboriosam
+          odit accusamus qui? blanditiis beatae eius, tenetur, enim animi
+          voluptate non asperiores excepturi explicabo deleniti! Ipsam optio
+          error est rem aperiam, placeat natus quibusdam officia vel aliquid
+          nesciunt aut nemo delectus sed sit architecto assumenda provident
+          dolorum. Alias sit eaque ad officia magni recusandae dignissimos
+          tenetur explicabo, est modi odio, doloremque vitae sequi eveniet
+          molestiae? Magnam nam sequi laboriosam odit accusamus qui? blanditiis
+          beatae e
+        </Text>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
